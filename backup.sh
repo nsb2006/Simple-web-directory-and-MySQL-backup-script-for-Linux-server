8b6916e55c9c58863dfd7fb43e9fb8b8c22e422e
nsb2006 <ucfresh@gmail.com>
2018-08-24 11:50:03 +0800

    backup.sh
    Main backup script

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Funciont: Backup website and mysql database
+#Author: Jason
+
+######~Set wwwroot and backup directory ~######
+Httpdocs_path='/home/wwwroot'
+Backup_tmp='/home/backup/tmp'
+Backup_path='/home/backup'
+Server_name='zz1'
+
+######~Set MySQL UserName, password, bin directory and database names ~######
+MYSQL_UserName='vlf35'
+MYSQL_PassWord='gtcuzgi9'
+MYSQL_path='/usr/local/mysql/bin'
+
+#Values Setting END!
+
+
+OldHttpdocsBackup=$Server_name-httpdocs-$(date -d -21day +"%Y%m%d").tar.gz
+OldDBBackup=$Server_name-db-$(date -d -21day +"%Y%m%d").tar.gz
+
+tar -cvzPf $Backup_path/$Server_name-httpdocs-$(date +"%Y%m%d").tar.gz $Httpdocs_path/
+
+$MYSQL_path/mysqldump -u$MYSQL_UserName -p$MYSQL_PassWord --all-databases >$Backup_tmp/%Server_name-db-all.sql 
+
+tar --remove-files -cvzf $Backup_path/$Server_name-db-$(date +"%Y%m%d").tar.gz $Backup_tmp/* >/dev/null  2>& 1
+
+
+rm -f $Backup_path/$OldHttpdocsBackup
+rm -f $Backup_path/$OldDBBackup
